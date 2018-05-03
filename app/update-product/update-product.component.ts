62e53fcdd4a546328e4ff17b11db3e3ea54d324c
slimane <boudaaslimane@gmail.com>
2018-05-03 15:42:03 +0200

    udate productcomponent

--- app/update-product/update-product.component.ts
+++ app/update-product/update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-update-product',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-product.component.css']
 })
 export class UpdateProductComponent implements OnInit {
+  product:Product;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+     this.productService = productService;
+   }
+   resetForm(form?: NgForm) {
+    if (form != null)
+      form.reset();
+    this.product =new Product(0,'','',0,'',0,'',false);
   }
 
+  ngOnInit() {
+    this.resetForm();
+
+      
+  }
+
+  
 }
+// OnSubmit(form: NgForm) {
+//   this.productService.update(form.value.productId)
+//   .subscribe(data => {
+//     this.resetForm(form);
+
+//   }
+
+// }
