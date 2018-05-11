d48cf263689b4633386499c2fbbf81db1b68ea73
Unknown <clement.birette@hotmail.Fr>
2018-05-11 16:24:39 +0200

    update new user

--- app/new-user/new-user.component.ts
+++ app/new-user/new-user.component.ts
@@ -14,6 +14,8 @@ export class NewUserComponent implements OnInit {
   user: User;
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
   msgs: Message[] = [];
+  checkCGU: string;
+  CGUPanelVisi: boolean = false;
 
   constructor(private userService: UserService) { }
 
@@ -29,7 +31,22 @@ export class NewUserComponent implements OnInit {
 
   OnSubmit(form: NgForm) {
     this.msgs = [];
-    console.log(this.user.phone);
+    if (!this.checkCGU) {
+      this.msgs.push({
+        severity: 'error',
+        summary: "CGU / CGV",
+        detail: "Veuillez accepter les CGU et CGV afin de créer votre compte."
+      });
+      return;
+    }
+    if (!form.valid) {
+      this.msgs.push({
+        severity: 'error',
+        summary: "Champs vides",
+        detail: "Tous les champs de ce formulaire sont obligatoires."
+      });
+      return;
+    }
     //check phone input is valid
     if (isNaN(parseInt(this.user.phone))) {
       this.msgs.push({
@@ -38,23 +55,26 @@ export class NewUserComponent implements OnInit {
         detail: 'Vous devez saisir un numéro de téléphone'
       });
     } else {
-      /* this.userService.registerUser(this.user)
-           .subscribe((data: any) => {
-             if (data.state == "success") {
-               this.resetForm(form);
-               this.msgs.push({
-                 severity: 'success',
-                 summary: "Votre compte a été créer",
-                 detail: 'Vous pouvez maintenant vous connecté'
-               });
-             } else {
-               this.msgs.push({
-                 severity: 'error',
-                 summary: "Erreur",
-                 detail: 'Cette addresse mail est déjà relié à un compte.'
-               });
-             }
-           }, error => console.log(error)); */
+      this.userService.registerUser(this.user)
+        .subscribe((data: any) => {
+          if (data.state == "success") {
+            this.resetForm(form);
+            this.msgs.push({
+              severity: 'success',
+              summary: "Votre compte a été créer",
+              detail: 'Vous pouvez maintenant vous connecté'
+            });
+          } else {
+            this.msgs.push({
+              severity: 'error',
+              summary: "Erreur",
+              detail: 'Cette addresse mail est déjà relié à un compte.'
+            });
+          }
+        }, error => console.log(error));
     }
+  }
+  showCGU() {
+    this.CGUPanelVisi = true;
   }
 }
